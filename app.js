0ef800685a66ba50c343d666a385675abcc19635
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-10-21 11:15:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ function criaCartao(categoria, pergunta, resposta) {
 
 
 cartao.innerHTML = `
-  <div class="cartao_conteudo">
-                  <h3>${categoria}</h3>
-                  <div class="cartao_conteudo_pergunta">
-                      <p>${pergunta}</p>
-                  </div>  
-                  <div class="cartao_conteudo_resposta">
-                    <p>${resposta}</p>
+   <div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>${pergunta}</p>
+                    </div>  
+                    <div class="cartao_conteudo_resposta">
+                        <p>${resposta}</p>
+                    </div>
                   </div>
-                </div>
                 `
     let respostaEstaVisivel = false;
 
