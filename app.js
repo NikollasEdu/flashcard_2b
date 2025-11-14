dfda05dfe1f04f1b3df3168c050d43b3355e630d
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-11-14 11:03:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ cartao.innerHTML = `
                     </div>
                 </div>
                 `
-   let respostaEstaVisivel = false;
+  let respostaEstaVisivel = false;
 
-   function viraCartao() {
-     respostaEstaVisivel = !respostaEstaVisivel
-     cartao.classList.toggle('active', respostaEstaVisivel)
-   }
-     cartao.addEventListener('click', viraCartao)
+  function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+  }
+    cartao.addEventListener('click', viraCartao)
 
-     container.appendChild(cartao)
+    container.appendChild(cartao)
 }
